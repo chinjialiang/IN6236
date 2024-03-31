7bfb418adda827fba9cd7a6816b1ad680af6ee8b
Chin Jia Liang <chinjialiang@live.com>
2024-03-31 21:10:59 +0800

    .

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract IdentityVerification {
-    struct Identity {
-        string fullName;
-        uint256 idNumber;
-        bool isVerified;
-        uint256 age;
-    }
+import "./Types.sol";
 
-    mapping(uint256 => Identity) public identities;
+contract IdentityVerification {
+
+    mapping(uint256 => Types.Identity) public identities;
 
     event IdentityVerified(uint256 indexed user);
 
@@ -29,16 +25,17 @@ contract IdentityVerification {
         require(bytes(_fullName).length > 0, "Full name must not be empty");
         require(_age >= 0, "Age must not be empty");
 
-        identities[_idNumber] = Identity({
+        identities[_idNumber] = Types.Identity({
             fullName: _fullName,
             idNumber: _idNumber,
             isVerified: false,
+            voted: false,
             age: _age
         });
     }
 
     function verifyIdentity(uint256 _idNumber) external {
-        Identity storage identity = identities[_idNumber];
+        Types.Identity storage identity = identities[_idNumber];
         require(bytes(identity.fullName).length > 0, "Identity not registered");
         require(identity.idNumber > 0, "Invalid ID number");
         require(!identity.isVerified, "Identity already verified");
@@ -49,27 +46,30 @@ contract IdentityVerification {
     }
 
     function getIdentity(uint256 _user) external view returns (string memory, uint256, bool, uint256) {
-        Identity memory identity = identities[_user];
+        Types.Identity memory identity = identities[_user];
         return (identity.fullName, identity.idNumber, identity.isVerified, identity.age);
     }
 
     function initializeVoterDatabase_() internal {
-        identities[uint256(11111)] = Identity({
+        identities[uint256(11111)] = Types.Identity({
             fullName: "Aby",
             idNumber: uint256(11111),
             isVerified: false,
+            voted: false,
             age:20
         });
-        identities[uint256(22222)] = Identity({
+        identities[uint256(22222)] = Types.Identity({
             fullName: "George",
             idNumber: uint256(22222),
             isVerified: false,
+            voted: false,
             age:50
         });
-        identities[uint256(33333)] = Identity({
+        identities[uint256(33333)] = Types.Identity({
             fullName: "Steve",
             idNumber: uint256(33333),
             isVerified: false,
+            voted: false,
             age:12
         });
     }
